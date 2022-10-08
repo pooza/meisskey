90d013285fd3012138acc1e100abe49668184014
mei23 <m@m544.net>
2022-10-09 08:10:56 +0900

    DNSリゾルブをランダムに

--- src/misc/dns.ts
+++ src/misc/dns.ts
@@ -5,25 +5,29 @@ type LookupOneCallback = (err: Error | null, address?: string, family?: number) 
 export function lookup(hostname: string, options: { family?: number, hints?: number }, callback: LookupOneCallback) {
 	if (options.family === 4) {
 		dns.promises.resolve4(hostname).then(addresses => {
-			callback(null, addresses[0], 4);
+			callback(null, pickRandom(addresses), 4);
 		}).catch(err => {
 			callback(err);
 		});
 	} else if (options.family === 6) {
 		dns.promises.resolve6(hostname).then(addresses => {
-			callback(null, addresses[0], 6);
+			callback(null, pickRandom(addresses), 6);
 		}).catch(err => {
 			callback(err);
 		});
 	} else {
 		dns.promises.resolve4(hostname).then(addresses => {
-			callback(null, addresses[0], 4);
+			callback(null, pickRandom(addresses), 4);
 		}).catch(err4 => {
 			dns.promises.resolve6(hostname).then(addresses => {
-				callback(null, addresses[0], 6);
+				callback(null, pickRandom(addresses), 6);
 			}).catch(err6 => {
 				callback(err6);
 			});
 		});
 	}
+}
+
+function pickRandom(s: string[]) {
+	return s[Math.floor(Math.random() * s.length)]
 }
