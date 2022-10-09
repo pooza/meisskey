70346457f0f66ca4ec32123c901241ce9dd0aa28
mei23 <m@m544.net>
2022-10-09 13:14:00 +0900

    1025 vCPU以上のマシンがあっても大丈夫なように

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
 Error.stackTraceLimit = Infinity;
 
 require('events').EventEmitter.defaultMaxListeners = 128;
-process.env.UV_THREADPOOL_SIZE = `${Math.max(4, require('os').cpus().length)}`;
+process.env.UV_THREADPOOL_SIZE = `${Math.min(Math.max(4, require('os').cpus().length), 1024)}`;
 
 import * as os from 'os';
 import * as cluster from 'cluster';
