c181d11d5fa651a5df67c6ab06c66a24bae253b6
mei23 <m@m544.net>
2022-10-10 13:20:51 +0900

    Fix lint

--- src/misc/is-objectid.ts
+++ src/misc/is-objectid.ts
@@ -4,5 +4,9 @@ import { ObjectID } from 'mongodb';
  * typeがObjectIDか、stringじゃダメ
  */
 export default function(x: any): x is ObjectID {
-	return x && typeof x === 'object' && (x.hasOwnProperty('toHexString') || x.hasOwnProperty('_bsontype'));
+	return x && typeof x === 'object' && (
+			Object.prototype.hasOwnProperty.call(x, 'toHexString') 
+			||
+			Object.prototype.hasOwnProperty.call(x, '_bsontype')
+		);
 }
