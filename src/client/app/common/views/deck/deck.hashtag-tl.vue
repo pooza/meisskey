f43bcc80df8b45a287ab7ce25a33fd9c3776f19f
mei23 <m@m544.net>
2022-11-29 23:43:03 +0900

    デッキのハッシュタグタイムラインがエラーになるのを修正 #4197

--- src/client/app/common/views/deck/deck.hashtag-tl.vue
+++ src/client/app/common/views/deck/deck.hashtag-tl.vue
@@ -35,7 +35,7 @@ export default Vue.extend({
 				includeMyRenotes: this.$store.state.settings.showMyRenotes,
 				includeRenotedMyNotes: this.$store.state.settings.showRenotedMyNotes,
 				includeLocalRenotes: this.$store.state.settings.showLocalRenotes,
-				tag: this.tagTl.tag
+				query: this.tagTl.query
 			}).then(notes => {
 				if (notes.length == fetchLimit + 1) {
 					notes.pop();
