0675d4961d58d5a854aaf7a2a2aef5bf718b20f2
mei23 <m@m544.net>
2022-12-19 01:13:41 +0900

    管理画面のリレーページが無駄に見れるのを修正

--- src/client/app/admin/views/index.vue
+++ src/client/app/admin/views/index.vue
@@ -25,7 +25,7 @@
 			<li><router-link to="/users" active-class="active" v-if="$store.getters.isAdminOrModerator"><fa icon="users" fixed-width/>{{ $t('users') }}</router-link></li>
 			<li><router-link to="/drive" active-class="active" v-if="$store.getters.isAdminOrModerator"><fa icon="cloud" fixed-width/>{{ $t('@.drive') }}</router-link></li>
 			<li><router-link to="/federation" active-class="active"><fa :icon="faGlobe" fixed-width/>{{ $t('federation') }}</router-link></li>
-			<li><router-link to="/relays" active-class="active"><fa :icon="faProjectDiagram" fixed-width/>{{ $t('relays') }}</router-link></li>
+			<li><router-link to="/relays" active-class="active" v-if="$store.getters.isAdminOrModerator"><fa :icon="faProjectDiagram" fixed-width/>{{ $t('relays') }}</router-link></li>
 			<li><router-link to="/emoji" active-class="active" v-if="$store.getters.isAdminOrModerator"><fa :icon="faGrin" fixed-width/>{{ $t('emoji') }}</router-link></li>
 			<li><router-link to="/announcements" active-class="active" v-if="$store.getters.isAdminOrModerator"><fa icon="broadcast-tower" fixed-width/>{{ $t('announcements') }}</router-link></li>
 			<li><router-link to="/hashtags" active-class="active" v-if="$store.getters.isAdminOrModerator"><fa icon="hashtag" fixed-width/>{{ $t('hashtags') }}</router-link></li>
