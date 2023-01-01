060e6eec942407669aa411f7b0a7ed583d0423dd
mei23 <m@m544.net>
2023-01-01 12:16:46 +0900

    SI接頭語を追加

--- src/client/app/common/views/filters/bytes.ts
+++ src/client/app/common/views/filters/bytes.ts
@@ -4,7 +4,7 @@ Vue.filter('bytes', (v, digits = 0) => {
 	if (v == null) return '?';
 	if (v == 0) return '0';
 	const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB',
-		'e+27B', 'e+30B', 'e+33B', 'e+36B', 'e+39B', 'e+42B', 'e+45B', 'e+48B'];
+		'RB', 'QB', 'e+33B', 'e+36B', 'e+39B', 'e+42B', 'e+45B', 'e+48B'];
 	const isMinus = v < 0;
 	if (isMinus) v = -v;
 	const i = Math.floor(Math.log(v) / Math.log(1024));
