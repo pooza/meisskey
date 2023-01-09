040f7f3cd89343087144bff65dc7c23e474f0aae
mei23 <m@m544.net>
2023-01-10 00:45:29 +0900

    api/emojis でリモート分などを削除

--- src/server/api/endpoints/emojis.ts
+++ src/server/api/endpoints/emojis.ts
@@ -1,9 +1,5 @@
-import $ from 'cafy';
 import define from '../define';
-import { fallback } from '../../../prelude/symbol';
 import Emoji, { packXEmoji } from '../../../models/emoji';
-
-const nonnull = { $ne: null as any };
 
 export const meta = {
 	tags: ['emojis'],
@@ -13,33 +9,6 @@ export const meta = {
 	cacheSec: 60,
 
 	params: {
-		limit: {
-			validator: $.optional.num.range(1, 5000),
-			default: 5000
-		},
-
-		offset: {
-			validator: $.optional.num.min(0),
-			default: 0
-		},
-
-		sort: {
-			validator: $.optional.str.or([
-				'+name',
-				'-name',
-				'+updatedAt',
-				'-updatedAt',
-			]),
-		},
-
-		origin: {
-			validator: $.optional.str.or([
-				'combined',
-				'local',
-				'remote',
-			]),
-			default: 'local'
-		}
 	},
 
 	res: {
@@ -50,34 +19,13 @@ export const meta = {
 	},
 };
 
-const origin: any = { // < https://github.com/Microsoft/TypeScript/issues/1863
-	'local': { host: null },
-	'remote': { host: nonnull },
-	[fallback]: {}
-};
-
-const sort: any = { // < https://github.com/Microsoft/TypeScript/issues/1863
-	'+name': { name: -1 },
-	'-name': { name: 1 },
-	'+updatedAt': { updatedAt: -1 },
-	'-updatedAt': { updatedAt: 1 },
-	[fallback]: { _id: -1 }
-};
-
 export default define(meta, async (ps, me) => {
 
 	const emojis = await Emoji
 		.find({
-			$and: [
-				origin[ps.origin] || origin[fallback]
-			],
-		}, {
-			limit: ps.limit,
-			sort: sort[ps.sort] || sort[fallback],
-			skip: ps.offset
+			host: null,
 		});
 
-	// TODO: v13はemojisの下にあるのでなおすかも
 	return {
 		emojis: await Promise.all(emojis.map(emoji => packXEmoji(emoji))),
 	};
