81b697e3262a964d7a4c7e6ba5b919a71870b624
mei23 <m@m544.net>
2023-03-02 04:16:01 +0900

    Fix

--- src/server/api/endpoints/notes/timeline.ts
+++ src/server/api/endpoints/notes/timeline.ts
@@ -1,5 +1,4 @@
 import $ from 'cafy';
-import * as mongo from 'mongodb';
 import ID, { transform } from '../../../../misc/cafy-id';
 import { getFriendIds } from '../../common/get-friends';
 import define from '../../define';
