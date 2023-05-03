5c4da26bfb87ec62194871f780cedd1ddedc43b5
mei23 <m@m544.net>
2023-05-04 01:11:50 +0900

    RJ番号でannounceだとリダイレクトされないことがあるのを修正 Fix #4611
    announceだった場合はURLプレビューから正しいURLにアクセスできる

--- src/mfm/language.ts
+++ src/mfm/language.ts
@@ -332,7 +332,7 @@ export const mfmLanguage = P.createLanguage({
 				return createMfmNode('link',
 				{
 					silent: false,
-					url: `https://www.dlsite.com/home/announce/=/product_id/${x}.html`,
+					url: `https://www.dlsite.com/home/work/=/product_id/${x}.html`,
 				}, [createMfmNode('text', { text: x })]);
 			}),
 			P.regexp(/([RVB][G](\d{5}))/, 1).map((x: any) => {
