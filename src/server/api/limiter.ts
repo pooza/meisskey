aa69e996fedb57f9451d5308afb0bcfcbe50672c
MeiMei <30769358+mei23@users.noreply.github.com>
2023-08-09 22:14:18 +0900

    async-ratelimiter (#4673)
    * async ratelimiter
    
    * split again

--- src/server/api/limiter.ts
+++ src/server/api/limiter.ts
@@ -1,97 +1,77 @@
-import * as Limiter from 'ratelimiter';
+import * as Limiter from 'async-ratelimiter';
 import limiterDB from '../../db/redis';
 import { IEndpoint } from './endpoints';
-import getAcct from '../../misc/acct/render';
 import { IUser } from '../../models/user';
 import Logger from '../../services/logger';
 import { addrToPeer } from '../../misc/addr-to-peer';
 
 const logger = new Logger('limiter');
 
-export default (endpoint: IEndpoint, user: IUser | null | undefined, ip?: string) => new Promise((ok, reject) => {
-	// Redisがインストールされてない場合は常に許可
-	if (limiterDB == null) {
-		ok();
-		return;
-	}
+/**
+ * Increment and check limit
+ * @returns Return if OK, otherwise throw.
+ */
+export async function incrementAndCheck(endpoint: IEndpoint, user: IUser | null | undefined, ip?: string) {
+	if (limiterDB == null) return;	// OK (Limiter DB unavailable)
 
 	const limitation = endpoint.meta.limit;
-	if (limitation == null) {
-		ok();
-		return;
-	}
+	if (limitation == null) return;	// OK (Limit undefined)
 
-	const target = user ? user._id : ip ? addrToPeer(ip) : null;
-	const targetName = user ? `@${getAcct(user)}` : ip ? addrToPeer(ip) : null;
+	// Prepare limiter
+	const target = genTargetKey(user, ip);
+	const key = genEpKey(endpoint);
 
-	const key = limitation.hasOwnProperty('key')
-		? limitation.key
-		: endpoint.name;
-
-	const hasShortTermLimit =
-		limitation.hasOwnProperty('minInterval');
-
-	const hasLongTermLimit =
-		limitation.hasOwnProperty('duration') &&
-		limitation.hasOwnProperty('max');
-
-	if (hasShortTermLimit) {
-		min();
-	} else if (hasLongTermLimit) {
-		max();
-	} else {
-		ok();
-	}
-
-	// Short-term limit
-	function min() {
-		const minIntervalLimiter = new Limiter({
-			id: `${target}:${key}:min`,
-			duration: limitation!.minInterval,
-			max: 1,
-			db: limiterDB!
-		});
-
-		minIntervalLimiter.get((err, info) => {
-			if (err) {
-				return reject('ERR');
-			}
-
-			logger.debug(`${targetName} ${endpoint.name} min remaining: ${info.remaining}`);
-
-			if (info.remaining === 0) {
-				reject('BRIEF_REQUEST_INTERVAL');
-			} else {
-				if (hasLongTermLimit) {
-					max();
-				} else {
-					ok();
-				}
-			}
-		});
-	}
-
-	// Long term limit
-	function max() {
+	// Check min interval (minimum attempt interval)
+	if (typeof limitation.minInterval === 'number') {
 		const limiter = new Limiter({
-			id: `${target}:${key}`,
-			duration: limitation!.duration,
-			max: limitation!.max,
-			db: limiterDB!
+			db: limiterDB,
+			duration: limitation.minInterval,
+			max: 1,
 		});
 
-		limiter.get((err, info) => {
-			if (err) {
-				return reject('ERR');
-			}
-
-			logger.debug(`${targetName} ${endpoint.name} max remaining: ${info.remaining}`);
-
-			if (info.remaining === 0) {
-				reject('RATE_LIMIT_EXCEEDED');
-			} else {
-				ok();
-			}
+		const info = await limiter.get({
+			id: `${target}:${key}:min`,
 		});
+
+		logger.debug(`${target} ${key} min remaining: ${info.remaining}`);
+
+		if (info.remaining === 0) throw new Error('BRIEF_REQUEST_INTERVAL');
 	}
-});
+
+	// Check max interval (general per-sec interval)
+	if (typeof limitation.duration === 'number' && typeof limitation.max === 'number') {
+		const limiter = new Limiter({
+			db: limiterDB,
+			duration: limitation.duration,
+			max: limitation.max,
+		});
+
+		const info = await limiter.get({
+			id: `${target}:${key}`,
+		});
+
+		logger.debug(`${target} ${key} max remaining: ${info.remaining}`);
+
+		if (info.remaining === 0) throw new Error('RATE_LIMIT_EXCEEDED');
+	}
+
+	return;	// OK (Passed all checks)
+}
+
+/**
+ * Generate target key
+ */
+function genTargetKey(user: IUser | null | undefined, ip: string | null | undefined): string | null {
+	if (user) return user._id;
+	if (ip) return addrToPeer(ip) ?? null;
+	return null;
+}
+
+/***
+ * Generate endpoint key
+ */
+function genEpKey(endpoint: IEndpoint) {
+	return endpoint.meta.limit?.key ?? endpoint.name;
+}
+
+export default incrementAndCheck;
